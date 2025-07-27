cf0eab489b503c2991bba0611fb6db94335526fc
priyamalviya831 <priyamalviya121212@gmail.com>
2025-07-27 17:54:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -147,13 +147,6 @@ import React from 'react';
 
 const Hero = () => {
     return (
-<>
-<style>{`
-    * {
-      outline: 1px solid red !important;
-    }
-  `}</style>
-        
   <div
     className="relative bg-gradient-to-bl from-black via-gray-900 to-black text-white  bg-cover bg-center bg-no-repeat overflow-hidden min-h-[100dvh]"
     style={{ backgroundImage: `url(${homebackground})
@@ -220,7 +213,6 @@ const Hero = () => {
       </a>
     </div>
   </div>
-    </>
 );
 };
 
